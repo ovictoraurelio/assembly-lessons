1cb3b7728aa9d549bd820d63dfddb631af94af46
Victor Aurelio Guimaraes Silva <vags@ug2c12.windows.cin.ufpe.br>
2017-03-18 14:37:45 -0300

    Hello World

--- helloObserveUniverse.asm
+++ helloObserveUniverse.asm
@@ -1,14 +1,15 @@
 org 0x7c00 ;this command 
 
+; ♥ ♥ ♥ ♥ ♥
+; Assembly is love 
+; @ovictoraurelio
+
+
 jmp 0x0000:start
 
-;intvideo:
-;	mov AH, 0xe
-;	mov BH, 13h
-;	int 10h
 
 string:
-	db "Hello Observe Universe", 0
+	db "Hello Observe Universe", 13, 10, 0
 
 start:
 	; ax is a reg to geral use
@@ -16,19 +17,18 @@ start:
 	mov ax,0 	;ax = 0
 	mov ds,ax 	;ds = 0
 	mov cl,0	;cl = 0
+
 	; load first memory postion of my string on SI (source)
 	mov si, string
 	
 	print:
-		lodsb
-		cmp cl,al
+		lodsb		; load al, si index and si++
+		cmp cl,al	; compare al with 0 (0, was set as end of string)
 		je end
-
-		;jmp intvideo	
 		
-		mov ah,0xe
-		mov bh, 13h
-		int 10h
+		mov ah,0xe	; instruction to show on screen
+		mov bh, 13h	
+		int 10h		; call video interrupt
 
 	jmp print
 
